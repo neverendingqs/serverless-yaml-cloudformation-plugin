0d63973cfc54c24ca82158c303b61a46f55b80c3
Mark Tse <mark.tse@neverendingqs.com>
2019-07-21 14:23:32 -0400

    plugin - init.

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class YamlCloudFormation {
+  constructor(serverless) {
+    this.provider = serverless.getProvider('aws');
+    this.hooks = {
+      'before:package:initialize': this.changeNaming.bind(this)
+    };
+  }
+
+  changeNaming() {
+    Object.assign(this.provider.naming, {
+      // https://github.com/serverless/serverless/blob/master/lib/plugins/aws/lib/naming.js
+      getServiceStateFileName: () => 'serverless-state.yaml',
+      getCoreTemplateFileName: () => 'cloudformation-template-update-stack.yaml',
+      getCompiledTemplateFileName: () => 'cloudformation-template-create-stack.yaml'
+    });
+  }
+}
+
+module.exports = YamlCloudFormation;
